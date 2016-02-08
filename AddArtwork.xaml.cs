c01d147c02f9a70dfe8a32fb635ce6dea316fa1c
Erik <erikstringberg@live.se>
2016-02-08 12:07:34 +0100

    En Början

---
+++ AddArtwork.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OGTavlor_MainProgram
+{
+    /// <summary>
+    /// Interaction logic for AddArtwork.xaml
+    /// </summary>
+    public partial class AddArtwork : Window
+    {
+        public AddArtwork()
+        {
+            InitializeComponent();
+        }
+    }
+}
