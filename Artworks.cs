2cd0b6afc954029b2110a786088afac7d1218dd0
Aquerr <nerdianin@gmail.com>
2016-02-08 14:43:25 +0100

    Changed class Artworks.cs

--- Artworks.cs
+++ Artworks.cs
@@ -6,34 +6,23 @@ using System.Threading.Tasks;
 
 namespace OGTavlor_MainProgram
 {
-    class Artworks
+    public class Artworks
     {
-        List<Artwork> Inventory;
-
         public Artworks()
         {
-            Inventory = new List<Artwork>();
-            Inventory.Add(new Artwork() { ArtworkId = 1, Artist = "Picasso", Title = "Guernica" });
-            Inventory.Add(new Artwork() { ArtworkId = 2, Artist = "Brutus", Title = "Pingviner" });
-            Inventory.Add(new Artwork() { ArtworkId = 3, Artist = "Brutus", Title = "Uggla" });
 
-            //Använd DataBindning för att koppla Inventory till kontrollen ni anv för att visa konstverken
-            //Alla operationer (lägg till, redigera, ta bort = CRUD) jobbar mot Inventory
-            //Senare är det enkelt att ersätta Inventory med en riktig DB (och till molnet)
-            //för en skateboard räcker detta
-
-            //EXTRA
-            //Kan vi se till att spara Inventory, när programmet avslutas alt ha en sparaknapp
-            //är ett stort plus!
-            //TIPS: Kolla hur man kan spara hela listan (Inventory) direkt till disk
-            //genom att serialisera den.
-            //https://msdn.microsoft.com/en-us/library/c5sbs8z9(v=vs.110).aspx
-            //exemplet anv en HashTable men vi har en List, annars samma...
-            //testa genom att skapa knappar för att spara och läsa in
-            //när programmet startar ska man då kunna läsa in data från fil
-            //och sedan spara uppdaterad data till fil
         }
+        public static List<Artwork> Invnetory = new List<Artwork>()
+         {
+             new Artwork() { ArtworkId = 1,Title = "Uggla", ImagePath = @"images\bild1mini.JPG" },
+             new Artwork() { ArtworkId = 2,Title = "Uggla", ImagePath = @"images\bild2mini.JPG" },
+             new Artwork() { ArtworkId = 3,Title = "Exempel", ImagePath = @"images\bild3mini.JPG" },
+             new Artwork() { ArtworkId = 4,Title = "Exempel", ImagePath = @"images\bild4mini.JPG" },
+             new Artwork() { ArtworkId = 5,Title = "Exempel", ImagePath = @"images\bild5mini.JPG" },
+             new Artwork() { ArtworkId = 6,Title = "Exempel", ImagePath = @"images\bild6mini.JPG" },
+       };
     }
+
 
     public class Artwork
     {
