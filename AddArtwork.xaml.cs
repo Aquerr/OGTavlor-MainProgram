cff9be69aec248677c9b6e5e5500e829a5d6aad5
Erik <erikstringberg@live.se>
2016-02-08 13:51:48 +0100

    Lagt till en preliminär AddImage_Click

--- AddArtwork.xaml.cs
+++ AddArtwork.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,6 @@ namespace OGTavlor_MainProgram
 
         List<Artwork> Inventory = new List<Artwork>();
         
-
         private void SaveArtwork_Click(object sender, RoutedEventArgs e)
         {           
             Inventory.Add(new Artwork() { Title = ArtName.Text, Artist = ArtistName.Text });
@@ -35,6 +35,23 @@ namespace OGTavlor_MainProgram
             MainWindow Main = new MainWindow();
             this.Close();
             Main.Show();
+        }
+
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
+            Artwork art = new Artwork();
+
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                ArtImage.Source = new BitmapImage(new Uri(op.FileName));
+                art.ImagePath = (ArtImage.Source as BitmapImage).UriSource.AbsolutePath;
+                //  MessageBox.Show(ImagePath);
+            }
         }
     }
 }
