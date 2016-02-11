20c7ca362b650c9cfe56526e1e969348fdcd0692
Erik <erikstringberg@live.se>
2016-02-11 11:27:43 +0100

    CmBxArtistName_SelectionChanged visar nu Konstnärerna, dock även dubletter

--- Artworks.cs
+++ Artworks.cs
@@ -14,12 +14,12 @@ namespace OGTavlor_MainProgram
         }
         public static List<Artwork> Invnetory = new List<Artwork>()
          {
-             new Artwork() { ArtworkId = 1,Title = "Uggla", ImagePath = @"images\bild1mini.JPG" },
-             new Artwork() { ArtworkId = 2,Title = "Uggla", ImagePath = @"images\bild2mini.JPG" },
-             new Artwork() { ArtworkId = 3,Title = "Exempel", ImagePath = @"images\bild3mini.JPG" },
-             new Artwork() { ArtworkId = 4,Title = "Exempel", ImagePath = @"images\bild4mini.JPG" },
-             new Artwork() { ArtworkId = 5,Title = "Exempel", ImagePath = @"images\bild5mini.JPG" },
-             new Artwork() { ArtworkId = 6,Title = "Exempel", ImagePath = @"images\bild6mini.JPG" },
+             new Artwork() { ArtworkId = 1,Title = "Uggla", ImagePath = @"images\bild1mini.JPG", Artist = "Kalle Johansson" },
+             new Artwork() { ArtworkId = 2,Title = "Uggla", ImagePath = @"images\bild2mini.JPG", Artist = "Kalle Johansson"},
+             new Artwork() { ArtworkId = 3,Title = "Exempel", ImagePath = @"images\bild3mini.JPG", Artist = "Sven Axelsson"},
+             new Artwork() { ArtworkId = 4,Title = "Exempel", ImagePath = @"images\bild4mini.JPG", Artist = "Glenn Göteborgsson"},
+             new Artwork() { ArtworkId = 5,Title = "Exempel", ImagePath = @"images\bild5mini.JPG", Artist = "Tim Timmerman"},
+             new Artwork() { ArtworkId = 6,Title = "Exempel", ImagePath = @"images\bild6mini.JPG", Artist = "Sven Axelsson"},
        };
     }
 
